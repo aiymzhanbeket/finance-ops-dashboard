f212505a24e68174da5aa8a7418f39274756fe07
aiymzhanbeket <aiymzhanbeket@gmail.com>
2026-07-23 23:59:13 +0500

    add transaction button js

---
+++ frontend/js/practice.js
@@ -0,0 +1,14 @@
+const transaction = { //creates one object 
+    amount: 4800,
+    category: "Sales",
+    type: "Revenue",
+    status: "Completed",
+};
+
+console.log(transaction);
+
+transaction.status = "Pending"; //to change the status
+// ":" to create an object
+// "=" to change an existing value 
+
+
